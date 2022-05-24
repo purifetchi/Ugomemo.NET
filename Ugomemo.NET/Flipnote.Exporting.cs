17598730aa773a9db9ad903d87e63b01e4b6ea9b
naomiEve <0xlunaric@gmail.com>
2022-05-24 13:33:03 +0200

    feat!: add GIF exporting.

---
+++ Ugomemo.NET/Flipnote.Exporting.cs
@@ -0,0 +1,28 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Ugomemo.NET
+{
+    public sealed partial class Flipnote
+    {
+        /// <summary>
+        /// Export this Flipnote to a GIF file.
+        /// </summary>
+        /// <param name="filename">The path to save the exported file.</param>
+        public void ExportToGIF(string filename)
+        {
+            var gif = new Image<Rgb24>(Animation.Frame.WIDTH, Animation.Frame.HEIGHT);
+            foreach (var frame in Frames)
+                gif.Frames.AddFrame(frame.Image.Frames[0]);
+
+            foreach (var frame in gif.Frames)
+                frame.Metadata.GetGifMetadata().FrameDelay = (int)(AnimationInfo.PlaybackSpeed * 100);
+
+            gif.Metadata.GetGifMetadata().RepeatCount = (ushort)(AnimationInfo.Looping ? 0 : 1);
+            gif.Metadata.GetGifMetadata().ColorTableMode = GifColorTableMode.Local;
+
+            gif.SaveAsGif(filename);
+        }
+    }
+}
